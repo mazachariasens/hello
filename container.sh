5dc6573940476d15a86cfe35cb62431050c19b5f
sebastian Oksby Dinesen <sebastianoksbydinesen@MacBook-Air-tilhrende-sebastian.local>
2026-09-24 10:55:16 +0200

    Fix container cleanup

--- container.sh
+++ container.sh
@@ -5,3 +5,6 @@ docker run -d --name my-nginx nginx
 docker exec my-nginx ls -la /usr/share/nginx/html
 docker logs my-nginx
 docker run -d --name my-nginx-2 nginx
+
+docker stop my-nginx my-nginx-2
+docker rm my-nginx my-nginx-2
